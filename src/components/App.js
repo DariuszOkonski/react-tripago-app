b450accd4781934d148c1183e5604dcc9431cbf9
Dariusz Okonski <darek200180@gmail.com>
2021-12-02 02:49:16 +0000

    instal material ui icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import '../assets/css/App.css';
+import SimpleBottomNabigation from './SimpleBottomNavigation.js';
 
 function App() {
   return (
     <div className="App">
-      
+
+    <SimpleBottomNabigation />
+
+
     </div>
   );
 }
