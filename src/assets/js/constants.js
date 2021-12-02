68439743fa07e26bd83b53aa27d1d9247d512aac
Dariusz Okonski <darek200180@gmail.com>
2021-12-02 02:29:04 +0000

    install material ui

---
+++ src/assets/js/constants.js
@@ -0,0 +1,17 @@
+export const IMAGES_LIST = [
+    "https://cdn.pixabay.com/photo/2020/02/16/21/07/krakow-4854806_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/21/17/34/las-vegas-1846684_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/11/07/19/19/aurora-borealis-1032523_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/09/05/16/11/dallas-1646947_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2021/08/07/18/36/city-6529108_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/30/17/11/houston-1631453_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/10/17/40/prague-3010407_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/28/20/34/water-3046088_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/05/08/23/36/the-ruins-of-the-759068_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/09/15/14/35/city-4478471_960_720.jpg"
+];
+
+export const LOCATIONS = [
+    "europa", "america"
+]
